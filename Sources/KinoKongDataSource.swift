7f4eab440076507d9922bcc4ee11cbde941f9ae8
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-09 12:34:34 -0400

    relax requirements to items

--- Sources/KinoKongDataSource.swift
+++ Sources/KinoKongDataSource.swift
@@ -146,11 +146,11 @@ class KinoKongDataSource: DataSource {
       for item in items {
         let movie = KinoKongMediaItem(data: ["name": ""])
 
-        movie.name = item.item?.name
-        movie.id = item.item?.id
-        movie.description = item.item?.description
-        movie.thumb = item.item?.thumb
-        movie.type = item.item?.type
+        movie.name = item.item.name
+        movie.id = item.item.id
+        movie.description = item.item.description
+        movie.thumb = item.item.thumb
+        movie.type = item.item.type
 
         newItems += [movie]
       }
