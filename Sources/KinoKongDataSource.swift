a1751cc27f3795c42bc5122acc49226d5c9f1fb8
Alexander Shvets <alexander.shvets@gmail.com>
2017-11-03 21:41:45 -0400

    add support for rx swift

--- Sources/KinoKongDataSource.swift
+++ Sources/KinoKongDataSource.swift
@@ -6,7 +6,7 @@ import RxSwift
 class KinoKongDataSource: DataSource {
   let service = KinoKongService.shared
 
-  override open func loadAsync(params: Parameters) throws -> Observable<[Any]> {
+  override open func load(params: Parameters) throws -> Observable<[Any]> {
     var items: Observable<[Any]> = Observable.just([])
 
     let selectedItem = params["selectedItem"] as? MediaItem
